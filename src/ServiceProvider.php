315ce49ceb095245146e5e6459dda12b1c14b036
Lee Hicks <leehicks@dreamfactory.com>
2017-10-26 13:45:11 -0400

    Upgrade swagger to OpenAPI 3.0 specification

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace DreamFactory\Core\Oidc;
 
+use DreamFactory\Core\Enums\LicenseLevel;
 use DreamFactory\Core\Oidc\Models\OidcConfig;
 use DreamFactory\Core\Oidc\Services\OIDC;
 use DreamFactory\Core\Services\ServiceManager;
@@ -13,22 +14,23 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     public function register()
     {
         // Add our service types.
-        $this->app->resolving('df.service', function (ServiceManager $df){
+        $this->app->resolving('df.service', function (ServiceManager $df) {
             $df->addType(
                 new ServiceType([
-                    'name'            => 'oidc',
-                    'label'           => 'OpenID Connect',
-                    'description'     => 'OpenID Connect service supporting SSO.',
-                    'group'           => ServiceTypeGroups::OAUTH,
-                    'config_handler'  => OidcConfig::class,
-                    'factory'         => function ($config){
+                    'name'              => 'oidc',
+                    'label'             => 'OpenID Connect',
+                    'description'       => 'OpenID Connect service supporting SSO.',
+                    'group'             => ServiceTypeGroups::OAUTH,
+                    'subscription_required' => LicenseLevel::SILVER,
+                    'config_handler'    => OidcConfig::class,
+                    'factory'           => function ($config) {
                         return new OIDC($config);
                     },
                     'access_exceptions' => [
                         [
                             'verb_mask' => 2,
                             'resource'  => 'sso',
-                        ]
+                        ],
                     ],
                 ])
             );
